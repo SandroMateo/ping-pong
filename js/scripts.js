109673a983efbc98928db420f86c5fcd7a2c9b14
Sandro Alvarez <sandroalvarez@yahoo.com>
2016-08-18 18:33:10 -0700

    give functionality to website by making function that creates array that counts up to user input number and substitutes ping and pong in when necessary

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,25 @@
+function pingPong(input) {
+  var pongArray = [];
+  for (var i = 1; i <= input; i++) {
+    if(i % 5 === 0 && i % 3 === 0) {
+      pongArray.push("ping-pong!");
+    }
+    else if(i % 5 === 0) {
+      pongArray.push("pong!");
+    }
+    else if(i % 3 === 0) {
+      pongArray.push("ping!")
+    }
+    else {
+      pongArray.push(i);
+    }
+  }
+  return pongArray;
+}
 $(function() {
   $("form").submit(function(event) {
     event.preventDefault();
-    var inputNumber = parseInt($("input-number").val());
+    var inputNumber = parseInt($("#input-number").val());
     var output = pingPong(inputNumber);
     $("#output").text(output);
   });
